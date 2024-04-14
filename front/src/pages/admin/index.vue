eae52224b3a8a8791f688b5a3bd44e7a12f07f83
Wheel-2021 <86990703+Wheel-2021@users.noreply.github.com>
2024-04-14 18:17:32 +0900

    管理者画面修正

--- front/src/pages/admin/index.vue
+++ front/src/pages/admin/index.vue
@@ -15,97 +15,99 @@ definePageMeta({
 });
 </script>
 <template>
-  <NuxtLayout name="custom">
-    <section class="bg-white py-12">
-      <h1 class="mb-4 text-3xl font-medium text-center">
-        管理者ダッシュボード
-      </h1>
-      <p class="text-center text-gray-400 text-sm leading-relaxed">
-        管理者専用の領域です。全権限が与えられているので、操作にご注意を。
-      </p>
-    </section>
-    <article class="contents__inner bg-gray-100 py-16 px-4">
-      <div
-        class="mx-auto p-4 overflow-hidden bg-white rounded-lg shadow-sm dark:bg-gray-800"
-      >
-        ここにメッセージが入る。
-      </div>
-      <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4 mt-4">
+  <div>
+    <NuxtLayout name="custom">
+      <section class="bg-white py-12">
+        <h1 class="mb-4 text-3xl font-medium text-center">
+          管理者ダッシュボード
+        </h1>
+        <p class="text-center text-gray-400 text-sm leading-relaxed">
+          管理者専用の領域です。全権限が与えられているので、操作にご注意を。
+        </p>
+      </section>
+      <article class="contents__inner bg-gray-100 py-16 px-4">
         <div
-          class="p-6 bg-white rounded-lg shadow-sm transition duration-300 ease-in-out hover:bg-sky-100"
+          class="mx-auto p-4 overflow-hidden bg-white rounded-lg shadow-sm dark:bg-gray-800"
         >
-          <NuxtLink
-            class="flex flex-col justify-center items-center"
-            to="/admin/users"
-          >
-            <UsersIcon class="w-6 h-6 mb-2 text-sky-500" />
-            ユーザー一覧
-          </NuxtLink>
+          ここにメッセージが入る。
         </div>
+        <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4 mt-4">
+          <div
+            class="p-6 bg-white rounded-lg shadow-sm transition duration-300 ease-in-out hover:bg-sky-100"
+          >
+            <NuxtLink
+              class="flex flex-col justify-center items-center"
+              to="/admin/users"
+            >
+              <UsersIcon class="w-6 h-6 mb-2 text-sky-500" />
+              ユーザー一覧
+            </NuxtLink>
+          </div>
 
+          <div
+            class="p-6 bg-white rounded-lg shadow-sm transition duration-300 ease-in-out hover:bg-sky-100"
+          >
+            <NuxtLink
+              class="flex flex-col justify-center items-center"
+              to="/dashboard/settings"
+            >
+              <Cog6ToothIcon class="w-6 h-6 mb-2 text-sky-500" />
+              設定変更
+            </NuxtLink>
+          </div>
+
+          <div
+            class="p-6 bg-white rounded-lg shadow-sm transition duration-300 ease-in-out hover:bg-sky-100"
+          >
+            <NuxtLink
+              class="flex flex-col justify-center items-center"
+              to="/admin/cancel"
+            >
+              <ListBulletIcon class="w-6 h-6 mb-2 text-sky-500" />
+              現在の退会申請一覧
+            </NuxtLink>
+          </div>
+
+          <div
+            class="p-6 bg-white rounded-lg shadow-sm transition duration-300 ease-in-out hover:bg-sky-100"
+          >
+            <NuxtLink
+              class="flex flex-col justify-center items-center"
+              to="/admin/pwreset"
+            >
+              <QueueListIcon class="w-6 h-6 mb-2 text-sky-500" />
+              パスワードリセット一覧
+            </NuxtLink>
+          </div>
+          <div
+            class="p-6 bg-white rounded-lg shadow-sm transition duration-300 ease-in-out hover:bg-sky-100"
+          >
+            <NuxtLink
+              class="flex flex-col justify-center items-center"
+              to="/dashboard"
+            >
+              <ComputerDesktopIcon class="w-6 h-6 mb-2 text-sky-500" />
+              ユーザー画面
+            </NuxtLink>
+          </div>
+        </div>
+      </article>
+
+      <article class="contents__inner bg-gray-100 h-screen py-16 px-4">
         <div
-          class="p-6 bg-white rounded-lg shadow-sm transition duration-300 ease-in-out hover:bg-sky-100"
+          class="w-8/12 min-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-sm dark:bg-gray-800"
         >
-          <NuxtLink
-            class="flex flex-col justify-center items-center"
-            to="/dashboard/settings"
-          >
-            <Cog6ToothIcon class="w-6 h-6 mb-2 text-sky-500" />
-            設定変更
-          </NuxtLink>
-        </div>
+          登録者一覧<br />
+          パスワードリセット一覧<br />
+          登録者退会(1週間後にDBから削除：論理削除から物理削除へ)
 
-        <div
-          class="p-6 bg-white rounded-lg shadow-sm transition duration-300 ease-in-out hover:bg-sky-100"
-        >
-          <NuxtLink
-            class="flex flex-col justify-center items-center"
-            to="/admin/cancel"
-          >
-            <ListBulletIcon class="w-6 h-6 mb-2 text-sky-500" />
-            現在の退会申請一覧
-          </NuxtLink>
+          <span v-if="currentUser">{{ currentUser.name }}</span>
+          <a href="/admin/sample">サンプルページ</a><br />
+          こんてんつ<br />
+          <a href="/dashboard/">ダッシュボード</a>
         </div>
-
-        <div
-          class="p-6 bg-white rounded-lg shadow-sm transition duration-300 ease-in-out hover:bg-sky-100"
-        >
-          <NuxtLink
-            class="flex flex-col justify-center items-center"
-            to="/admin/pwreset"
-          >
-            <QueueListIcon class="w-6 h-6 mb-2 text-sky-500" />
-            パスワードリセット一覧
-          </NuxtLink>
-        </div>
-        <div
-          class="p-6 bg-white rounded-lg shadow-sm transition duration-300 ease-in-out hover:bg-sky-100"
-        >
-          <NuxtLink
-            class="flex flex-col justify-center items-center"
-            to="/dashboard"
-          >
-            <ComputerDesktopIcon class="w-6 h-6 mb-2 text-sky-500" />
-            ユーザー画面
-          </NuxtLink>
-        </div>
-      </div>
-    </article>
-
-    <article class="contents__inner bg-gray-100 h-screen py-16 px-4">
-      <div
-        class="w-8/12 min-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-sm dark:bg-gray-800"
-      >
-        登録者一覧<br />
-        パスワードリセット一覧<br />
-        登録者退会(1週間後にDBから削除：論理削除から物理削除へ)
-
-        <span v-if="currentUser">{{ currentUser.name }}</span>
-        <a href="/admin/sample">サンプルページ</a><br />
-        こんてんつ<br />
-        <a href="/dashboard/">ダッシュボード</a>
-      </div>
-    </article>
-  </NuxtLayout>
+      </article>
+    </NuxtLayout>
+  </div>
 </template>
 <style lang="scss" scoped></style>
---
+++ front/src/pages/admin/pwreset.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+definePageMeta({
+  middleware: 'admin',
+  layout: false,
+});
+</script>
+<template>
+  <div>
+    <NuxtLayout name="custom">
+      <article class="contents__inner bg-gray-100 h-screen py-16 px-4">
+        <div
+          class="w-8/12 min-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-sm dark:bg-gray-800"
+        >
+          <a href="/admin/">管理画面トップページ</a>
+          こんてんつ
+        </div>
+      </article>
+    </NuxtLayout>
+  </div>
+</template>
+<style lang="scss" scoped></style>
