adb08824d18ba46875b851c37634f38b01e699ad
Wheel-2021 <muguruma@sixwheel.net>
2024-04-16 17:42:48 +0900

    管理者ユーザー一覧作業中

--- front/src/pages/admin/users.vue
+++ front/src/pages/admin/users.vue
@@ -1,8 +1,35 @@
 <script lang="ts" setup>
+import { UserCircleIcon } from '@heroicons/vue/24/outline';
+import { useAuthUser, useAuth } from '@/composables/auth';
+import { useField, useForm } from 'vee-validate';
+import { object, string } from 'yup';
 import { useAdmini } from '@/composables/admin';
+import useErrorHandler from '@/composables/useErrorHandler';
+import { prepareFormData } from '@/utils/prepareImageFormData';
+import type { User } from '@/types/user';
+
 const { getAllUsers } = useAdmini();
 
 const users = ref();
+const serverMessage = ref();
+
+let formData = new FormData();
+
+const schema = object({
+  mail: string().email('メールアドレスの形式ではありません'),
+  animal: string().matches(/^[^ -~｡-ﾟ]/, {
+    message: '漢字・カタカナ・ひらがなを全角で入力してください',
+  }),
+  password: string().min(10, '10文字以上で入力してください'),
+});
+const { errors, handleSubmit } = useForm({
+  validationSchema: schema,
+});
+
+const { value: fieldName, handleChange: handleChangeName } = useField('name');
+const { value: fieldMail, handleChange: handleChangeMail } = useField('mail');
+const { value: fieldAnimal, handleChange: handleChangeAnimal } =
+  useField('animal');
 
 const allDBUsers = async () => {
   try {
@@ -14,10 +41,10 @@ const allDBUsers = async () => {
   }
 };
 
+const submit = () => {};
+
 onMounted(async () => {
-  // users.value = await allDBUsers();
-  console.log('users');
-  console.log('users.vue', allDBUsers());
+  users.value = await allDBUsers();
 });
 
 definePageMeta({
@@ -35,19 +62,78 @@ definePageMeta({
         </p>
       </section>
       <article class="contents__inner bg-gray-100 py-16 px-4">
-        <div
+        <section
           class="mx-auto p-4 overflow-hidden bg-white rounded-lg shadow-sm dark:bg-gray-800"
         >
-          <dl v-for="(user, index) in users" :key="index">
-            <dt>{{ user.id }}</dt>
-            <dd>{{ user.name }}</dd>
-            <dd>{{ user.mail }}</dd>
-            <dd>{{ user.password }}</dd>
-            <dd>{{ user.filename }}</dd>
-            <dd>{{ user.role }}</dd>
-            <dd>{{ user.deleted }}</dd>
-          </dl>
-        </div>
+          <div class="relative h-screen pt-16 overscroll-y-auto">
+            <dl
+              class="absolute top-0 left-0 items-center w-full bg-white border-b font-normal"
+            >
+              <dt class="w-20 p-2 text-center">#</dt>
+              <dt class="w-40 p-2 text-center">名前</dt>
+              <dt class="w-50 p-2 text-center">メールアドレス</dt>
+              <dt class="w-32 p-2 text-center">動物名</dt>
+              <dt class="w-40 p-2 text-center">権限</dt>
+              <dt class="w-40 p-2 text-center">削除</dt>
+              <dt class="p-2 text-center">操作</dt>
+            </dl>
+
+            <dl
+              class="w-full border-b"
+              v-for="(user, index) in users"
+              :key="index"
+            >
+              <dt class="w-20 p-2 text-right">
+                {{ user.id }}
+              </dt>
+
+              <dd class="w-40 p-2 text-left">
+                <NuxtImg
+                  v-if="user.filename"
+                  :src="user.filename"
+                  width="30"
+                  class=""
+                  :alt="user.name || ''"
+                />
+                <UserCircleIcon
+                  v-else
+                  stroke-width="0.1"
+                  class="w-8 h-8text-gray-500"
+                />
+                <span class="ml-2">{{ user.name }}</span>
+              </dd>
+              <dd class="w-50 p-2 text-left">
+                {{ user.mail }}
+              </dd>
+              <dd class="w-32 p-2 text-left">
+                {{ user.animal }}
+              </dd>
+              <dd class="w-40 p-2 text-left">
+                <span v-if="user.role === 'admin'">管理者</span>
+                <span v-if="user.role === 'user'">一般ユーザー</span>
+              </dd>
+              <dd class="w-40 p-2 text-center">
+                <span v-if="user.delated">削除対象</span>
+                <span v-else>普通だよ</span>
+              </dd>
+              <dd class="p-2 text-center">
+                <button
+                  class="mr-1 px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
+                  type="submit"
+                >
+                  編集
+                </button>
+                <button
+                  class="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
+                  type="submit"
+                >
+                  削除
+                </button>
+              </dd>
+            </dl>
+            <div class="divide-dashed"></div>
+          </div>
+        </section>
       </article>
     </NuxtLayout>
   </div>
